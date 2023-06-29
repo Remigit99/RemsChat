be10ada93aa8a46047c6df7c8f4df388616f9210
Remigit99 <aderemibiodun1000@gmail.com>
2023-06-29 12:35:37 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h2>Welcome to Remchat</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
